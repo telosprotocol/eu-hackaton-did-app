1d5c51d0ad175523f5f91f5095c0bed1fe244a22
Slava <dolhov.viacheslav@globaltechmakers.com>
2019-09-28 15:22:42 +0300

    add registartion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,9 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './auth/register/register.module#RegisterPageModule' }
 ];
 
 @NgModule({
